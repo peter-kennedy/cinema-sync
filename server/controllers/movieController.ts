149382b38b8422c787adef8294bda3b49077bf6c
Peter Kennedy <pkennedy97@gmail.com>
2023-01-28 14:37:52 -0500

    adds getWatched controller

--- server/controllers/movieController.ts
+++ server/controllers/movieController.ts
@@ -1,8 +1,8 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction, request } from 'express';
 import { QueryResult } from 'pg';
 import query from '../models/model.js';
 
-interface Watchlist {
+interface Movie {
   id: number;
   title: string;
   title_card: string;
@@ -14,7 +14,7 @@ const movieController = {
 
     try {
       const VALUES: Array<unknown> = [id];
-      const data: QueryResult<Watchlist> = await query(
+      const data: QueryResult<Movie> = await query(
         `SELECT id, title, title_card 
       FROM movies m 
       inner join watchlist w 
@@ -30,6 +30,31 @@ const movieController = {
         log: `Error in movieController.getWatchlist: ERROR: ${err}`,
         status: 500,
         message: 'Counld not get watchlist',
+      });
+    }
+  },
+
+  async getWatched(req: Request, res: Response, next: NextFunction) {
+    const { id } = req.params;
+
+    try {
+      const VALUES: Array<unknown> = [id];
+      const data: QueryResult<Movie> = await query(
+        `SELECT id as movie_id, title, title_card 
+        FROM movies m 
+        inner join watched w 
+        on w.movie_id = m.id 
+        where w.user_id = $1`,
+        VALUES,
+        true
+      );
+      res.locals.watchedMovies = data;
+      return next();
+    } catch (err) {
+      return next({
+        log: `Error in movieController.getWatched: ERROR: ${err}`,
+        status: 500,
+        message: 'Counld not get watched movies',
       });
     }
   },
