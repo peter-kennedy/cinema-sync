e3d5aed676e7fdc0f880e99f27c19f047c6cce91
Peter Kennedy <pkennedy97@gmail.com>
2023-02-02 14:23:02 -0500

    adds testing file

---
+++ server/controllers/watchlistController.test.ts
@@ -0,0 +1,11 @@
+import httpMocks from 'node-mocks-http';
+import query from '../models/model.js';
+import watchlistController from './watchlistController.js';
+
+describe('watchlistController', () => {
+  describe('moveToWatched', () => {
+    it('should move a movie from the watchlist table to the watched table', async () => {});
+    it('should add a movie to the movies table if it is not already there', async () => {});
+    
+  });
+});
